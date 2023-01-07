84421ed00f86942d4ee775cb02ed24bdefec38e0
Bruno de Jesus Pinto <bruno.jpinto@outlook.com>
2023-01-07 10:36:22 -0300

    CRUD implementado

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from typing import Optional
+from flask import Flask, request, jsonify
 from flask_pydantic_spec import FlaskPydanticSpec, Response, Request
 from pydantic import BaseModel
 from tinydb import TinyDB, Query
@@ -9,18 +10,25 @@ spec = FlaskPydanticSpec('flask', title='Live de Python')
 spec.register(server)
 database = TinyDB('database.json')
 
-
 class Pessoa(BaseModel):
-    id: int
+    id: Optional[int]
     idade: int
     nome: str
 
+class Pessoas(BaseModel):
+    pessoas: list[Pessoa]
+    count: int
 
 @server.get('/pessoas')
+@spec.validate(resp=Response(HTTP_200=Pessoas))
 def buscar_pessoa():
     """Retorna todas as pessoas da base de dados"""
-    return database.all()
-
+    return jsonify(
+        Pessoas(
+            pessoas=database.all(),
+            count=len(database.all())
+        ).dict()
+    )
 
 @server.post('/pessoas')
 @spec.validate(body=Request(Pessoa), resp=Response(HTTP_200=Pessoa))
@@ -30,5 +38,21 @@ def inserir_pessoa():
     database.insert(body)
     return body
 
+@server.put('/pessoas/<int:id>')
+@spec.validate(body=Request(Pessoa), resp=Response(HTTP_200=Pessoa))
+def alterar_pessoa(id):
+    """Altera uma pessoa no banco de dados a partir do id"""
+    Pessoa = Query()
+    body = request.get_json()
+    database.update(body, Pessoa.id == id)
+    return jsonify(body)
+
+@server.delete('/pessoas/<int:id>')
+@spec.validate(resp=Response('HTTP_204'))
+def excluir_pessoa(id):
+    """Exclui uma pessoa no banco de dados a partir do id"""
+    Pessoa = Query()
+    database.remove(Pessoa.id == id)
+    return jsonify({})
 
 server.run()
